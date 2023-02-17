51525e73d6789978cb7bf159e1f766c98c4b0ab0
Andres Velasquez <andresvel00@gmail.com>
2023-02-17 22:29:41 +0000

    email validations

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use sqlx::postgres::PgPoolOptions;
+use zero2prod::email_client::EmailClient;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
@@ -14,11 +15,20 @@ async fn main() -> std::io::Result<()> {
         .acquire_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.database.with_db());
 
+    let sender_email = configuration.email_client.sender().expect("Invalid sender email address.");
+    let email_client = EmailClient::new(
+        configuration.email_client.base_url,
+        sender_email
+    );
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
+
     let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    run(listener, connection_pool)?.await?;
+
+    run(listener, connection_pool, email_client)?.await?;
+    
     Ok(())
 }
